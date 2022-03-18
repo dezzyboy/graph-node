ddea772da0e6afde8809a58f7fc04c9e9273a8d7
David Lutterkort <lutter@watzmann.net>
2022-03-18 16:51:33 -0700

    graph, store: Return panics from the background writer as errors

--- store/postgres/tests/store.rs
+++ store/postgres/tests/store.rs
@@ -1652,6 +1652,7 @@ fn handle_large_bytea_with_index() {
         let stopwatch_metrics = StopwatchMetrics::new(
             Logger::root(slog::Discard, o!()),
             deployment.hash.clone(),
+            "test",
             metrics_registry.clone(),
         );
 
